f554b5b2e9d5c5058d89b67133a8d106dc1398c3
yayanyang <yayanyang@gmail.com>
2016-02-18 12:47:13 +0800

    update

--- .gsmake.lua
+++ .gsmake.lua
@@ -1,26 +1,23 @@
 name "github.com/lemonkit/lime"
 
-version "develop"
-
-
 plugin "github.com/gsmake/clang"
 
 plugin "github.com/gsmake/lua"
 
 
 
-lua = {
-    dependencies    = {
-        { name = "github.com/lemonkit/lemoon", version = "develop" };
+properties.lua = {
+    dependencies        = {
+        { name = "github.com/lemonkit/lemoon" };
     };
 }
 
 
-clang = {
+properties.clang = {
     ["lime"] = {
         type            = "static";
         dependencies    = {
-            { name = "github.com/lemonkit/lemon", version = "develop" };
+            { name = "github.com/lemonkit/lemon" };
         };
     }
 }
